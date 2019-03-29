6924a4f94bad2faf40ef3348e117b46cfcacef82
besteternally <1215038665@qq.com>
2019-03-29 11:46:33 +0800

    add-user-permission

--- src/main/java/com/demo/controller/UsersController.java
+++ src/main/java/com/demo/controller/UsersController.java
@@ -3,9 +3,6 @@ package com.demo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.demo.bean.Users;
